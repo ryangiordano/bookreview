5cfe8028fcbc965329599fb747031c66124021c6
Ryan Giordano <Ryan Giordano>
2016-05-14 10:02:52 -0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('Lozone',[]);
+
+//controller for the book list
+app.controller('bookshelf', function('$scope'){
+
+});
+//add a book to the list
+
+
+//add a review
---
+++ js/controllers/mainController.js
@@ -0,0 +1,4 @@
+app.controller('mainController',['$scope',function($scope){	
+
+}]);
+
